194c3646ed5e4e998e7a68e44eea719569edc6bd
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-20 19:17:52 -0300

    Validando tipos aceitos de requisições

--- api/index.js
+++ api/index.js
@@ -5,10 +5,28 @@ const roteador = require("./rotas/fornecedores");
 const CampoInvalido = require("./erros/CampoInvalido");
 const DadosNaoFornecidos = require("./erros/DadosNaoFornecidos");
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
+const formatosAceitos = require("./Serializador").formatosAceitos;
 
 const app = express();
 
 app.use(express.json());
+
+app.use((request, response, next) => {
+    let formatoRequisitado = request.header("Accept");
+
+    if (formatoRequisitado === "*/*") {
+        formatoRequisitado = "application/json";
+    }
+
+    if (formatosAceitos.indexOf(formatoRequisitado) === -1) {
+        response.sendStatus(406);
+        response.end();
+        return
+    }
+
+    response.setHeader("Content-Type", formatoRequisitado);
+    next();
+});
 
 app.use("/api/fornecedores", roteador);
 
