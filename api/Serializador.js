f0e52da004bf558e8db5c213ad0a619b223876f4
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-20 20:27:45 -0300

    Protegendo dados

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,3 +1,4 @@
+const DadosNaoFornecidos = require("./erros/DadosNaoFornecidos");
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado");
 
 class Serializador {
@@ -7,10 +8,37 @@ class Serializador {
 
     serializar(dados) {
         if (this.contentType === "application/json") {
-            return this.json(dados);
+            return this.json(
+                this.filtrar(dados)
+            );
         }
 
         throw new ValorNaoSuportado(this.contentType);
+    }
+
+    filtrarObjeto(dados) {
+        const novoObjeto = {};
+
+        this.camposPublicos.forEach(campo => {
+            if (dados.hasOwnProperty(campo)) {
+                novoObjeto[campo] = dados[campo]
+            }
+        })
+
+        return novoObjeto;
+
+    }
+
+    filtrar(dados) {
+        if (Array.isArray(dados)) {
+            dados = dados.map(item => {
+                return this.filtrarObjeto(item);
+            })
+        } else {
+            dados = this.filtrarObjeto(dados);
+        }
+
+        return dados;
     }
 }
 
@@ -18,6 +46,7 @@ class SerializadorFornecedor extends Serializador {
     constructor(contentType) {
         super()
         this.contentType = contentType;
+        this.camposPublicos = ['id', 'empresa', 'categoria'];
     }
 }
 
