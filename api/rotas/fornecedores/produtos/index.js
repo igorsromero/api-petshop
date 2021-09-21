9ca7cd4f851c83f3d0f8ee11e3b815095672f904
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-21 15:56:11 -0300

    Remover produtos e tratando erros

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,37 @@
 const roteador = require("express").Router({ mergeParams: true });
 const Tabela = require("./TabelaProduto");
+const Produto = require("./Produto");
 
 roteador.get("/", async (request, response) => {
     const produto = await Tabela.listar(request.params.idFornecedor);
     response.send(
-        JSON.stringify([])
+        JSON.stringify(produto)
     )
+});
+
+roteador.post("/", async (request, response, next) => {
+    try {
+        const idFornecedor = request.params.idFornecedor;
+        const corpo = request.body;
+        const dados = Object.assign({}, corpo, { fornecedor: idFornecedor });
+        const produto = new Produto(dados);
+        await produto.criar();
+        response.status(201);
+        response.send(produto);
+    } catch (erro) {
+        next(erro);
+    }
+});
+
+roteador.delete("/:id", async (request, response) => {
+    const dados = {
+        id: request.params.id,
+        fornecedor: request.params.idFornecedor
+    }
+    const produto = new Produto(dados);
+    await produto.apagar();
+    response.status(204);
+    response.send(produto);
 });
 
 module.exports = roteador;
