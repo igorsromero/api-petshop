e7a9dabd699daae88c0b1f3219baadce577f6268
Igor Solfa Romero <igorsolfaromero@gmail.com>
2021-09-21 15:32:59 -0300

    Store e hierarquia

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,6 +1,8 @@
-const roteador = require("express").Router();
+const roteador = require("express").Router({ mergeParams: true });
+const Tabela = require("./TabelaProduto");
 
-roteador.get("/", (request, response) => {
+roteador.get("/", async (request, response) => {
+    const produto = await Tabela.listar(request.params.idFornecedor);
     response.send(
         JSON.stringify([])
     )
